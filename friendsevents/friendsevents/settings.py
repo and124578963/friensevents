c650f0f6d717d00e4aea76b6431acebca12a5228
Andrey Pervushin <89476926+and124578963@users.noreply.github.com>
2022-08-20 22:40:21 +0300

    Update settings.py

--- friendsevents/friendsevents/settings.py
+++ friendsevents/friendsevents/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%-xjz^_e_$ytrq@3r14)m49f^hb9&r@#lkik6@qlkq0r4775##'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
